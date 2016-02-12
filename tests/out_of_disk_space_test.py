15d3055d6c5be4a79e69435f56d86f3182a2f535
Raphael S. Carvalho <raphaelsc@scylladb.com>
2016-02-12 15:48:37 -0200

    introduce test to check scylla when it runs out of disk space
    Signed-off-by: Raphael S. Carvalho <raphaelsc@scylladb.com>

---
+++ tests/out_of_disk_space_test.py
@@ -0,0 +1,88 @@
+import sys, glob
+
+from subprocess import check_output
+import subprocess
+import signal
+import random
+import time
+import sys
+import os
+
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+
+from common import *
+
+def stress():
+    FNULL = open(os.devnull, 'w')
+    proc = subprocess.Popen(['../scylla-tools-java/tools/bin/cassandra-stress',
+                             'write',
+                             'duration=15m',
+                             '-mode',
+                             'native',
+                             'cql3',
+                             '-rate',
+                             'threads=700',
+                             '-node',
+                             'localhost'],
+                             preexec_fn=os.setsid,
+                             stdout=FNULL,
+                             stderr=subprocess.STDOUT)
+    return proc
+
+def cleanup(scylla_proc, scylla_log, stress_proc):
+    print "stopping cassandra stress"
+    os.killpg(os.getpgid(stress_proc.pid), signal.SIGTERM)
+    stress_proc.wait()
+
+    stop_scylla(scylla_proc, scylla_log)
+
+def main():
+    client = connect()
+    client.clear_all_faults()
+
+    scylla_proc, scylla_log = start_scylla("out-of-disk-space", True)
+
+    print "starting cassandra stress"
+    stress_proc = stress()
+    # Sufficient time for activity such as memtable write, compaction, etc.
+    time.sleep(random.randint(20, 50))
+
+    methods = client.get_methods()
+    ENOSPC = 28 # no space left on device error
+    print "creating out of disk space scenario"
+    client.set_fault(methods, False, ENOSPC, 100000, "", False, 0, False)
+
+    # Leave system with no space left for a few seconds.
+    time.sleep(20)
+
+    # Check that the system faced the  scenario
+    if not has_message("out-of-disk-space", b'No space left on device'):
+        print "Failed to make system run out of disk space"
+        cleanup(scylla_proc, scylla_log, stress_proc)
+        sys.exit(1)
+    print "System is out of disk space as expected"
+
+    # Clean faults and let the system recover from the out of disk space scenario.
+    print "destroying out of disk space scenario"
+    client.clear_all_faults()
+    scylla_log.seek(0)
+    scylla_log.truncate()
+    time.sleep(20)
+
+    # Checking that the system was actually able to recover
+    if has_message("out-of-disk-space", b'No space left on device'):
+        print "Failed to make system recover from out of disk space scenario"
+        cleanup(scylla_proc, scylla_log, stress_proc)
+        sys.exit(1)
+    print "System is no longer out of disk space as expected"
+
+    cleanup(scylla_proc, scylla_log, stress_proc)
+
+    print "Success"
+    print ""
+
+if __name__ == "__main__":
+    main()
