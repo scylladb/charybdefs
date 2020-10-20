7297a5436378775d5d3270109e3f57c400c2c7c9
Jackson Owens <jackson@cockroachlabs.com>
2020-10-20 17:07:56 -0400

    Allow specifying a set of errnos to select from.
    Allow setting a set of errnos rather than just a particular errno or a
    random one from the entire set. Update the cookbook for random faults to
    exclude any errnos passed via extra arguments.
    
    My objective here is to be able to exclude a specific errno from
    random injection. The Go runtime gets confused by EAGAINs, which cause
    it to epoll_wait on the file descriptor. I'd like to exclude EAGAIN from
    the set of injected errors for my use case.

--- python_client.py
+++ python_client.py
@@ -19,8 +19,8 @@ try:
 
     print(client.get_methods())
 
-    # client.set_fault(['flush', 'fsync', 'fsyncdir'], False, 0, 100000, "", True, 500000)
-    client.set_fault(['flush', 'fsync', 'fsyncdir'], False, 0, 99000, "", True, 500000)
+    # client.set_fault(['flush', 'fsync', 'fsyncdir'], False, [], 100000, "", True, 500000)
+    client.set_fault(['flush', 'fsync', 'fsyncdir'], False, [], 99000, "", True, 500000)
     # client.clear_all_faults()
 
 except Thrift.TException as tx:
